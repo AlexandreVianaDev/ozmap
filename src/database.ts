51a6c8f95d805aadffd30a80854f7d4f861b0ddc
AlexandreVianaDev <alexandreviana94@hotmail.com>
2024-11-02 12:34:35 -0300

    feature: inicializar banco e api

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const env = {
-  MONGO_URI: 'mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin',
+  MONGO_URI:
+    "mongodb://root:example@127.0.0.1:27021/oz-tech-test?authSource=admin",
 };
 
-const init = async function() {
-  await mongoose.connect(env.MONGO_URI);
+const pino = require("pino");
+const logger = pino({ level: "info" });
+
+const init = async function () {
+  try {
+    logger.info("Connecting to MongoDB");
+    await mongoose.connect(env.MONGO_URI);
+    logger.info("MongoDB connected");
+  } catch (e) {
+    logger.error("Error connecting to MongoDB:", e);
+  }
 };
 
-export default init();
+export default init;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-import './database';
+import init from "./database";
+import server from "./server";
+
+const pino = require("pino");
+const logger = pino({ level: "info" });
+
+(async () => {
+  await init();
+  server;
+  logger.info("Server is running");
+})();
